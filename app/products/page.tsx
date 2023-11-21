2c79e44e89d3585f6a253834606a941084a6a962
Daniil848 <ladygindaniil848@gmail.com>
2023-11-21 14:45:25 +0300

    install axios

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Products() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>home</p>
+      <p>products</p>
     </main>
   )
 }
